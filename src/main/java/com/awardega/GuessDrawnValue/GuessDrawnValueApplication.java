c21849ae888473fdf60ebd33e31fca12460f38c8
Agnieszka Wardęga <agnieszka.wardega@edu.uekat.pl>
2022-05-22 20:01:07 +0200

    Som upgrades

--- src/main/java/com/awardega/GuessDrawnValue/GuessDrawnValueApplication.java
+++ src/main/java/com/awardega/GuessDrawnValue/GuessDrawnValueApplication.java
@@ -1,9 +1,16 @@
 package com.awardega.GuessDrawnValue;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.awardega.GuessDrawnValue")
+@EnableAutoConfiguration(exclude= DataSourceAutoConfiguration.class)
+@EntityScan("com.awardega.GuessDrawnValue.entities")
 public class GuessDrawnValueApplication {
 
 	public static void main(String[] args) {
