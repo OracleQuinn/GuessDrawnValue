9255f97e0da299f216bbe994aaecb9d9ee22ff4f
Agnieszka Wardęga <agnieszka.wardega@edu.uekat.pl>
2022-05-22 18:59:44 +0200

    Initial commit

---
+++ src/main/java/com/awardega/GuessDrawnValue/GuessDrawnValueApplication.java
@@ -0,0 +1,13 @@
+package com.awardega.GuessDrawnValue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessDrawnValueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessDrawnValueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awardega/GuessDrawnValue/GuessDrawnValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awardega.GuessDrawnValue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessDrawnValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
